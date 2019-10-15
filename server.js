ab128948747e1dbe49489d9a5ad458ca234d5af0
Andrew Duong <duongsters@gmail.com>
2019-10-14 18:30:38 -0700

    updated my server.js file so it's sequelized....changed listening port, removed burgers_controller.js route to apiRoute.js route, added location of models folder for syncing

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -23,14 +26,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them
-var burgerRoutes = require("./controllers/burgers_controller.js");
-
-app.use(burgerRoutes);
+require("./routes/apiRoutes")(app);
 
 
 //--------------Listenng PORT---------------------------------//
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
